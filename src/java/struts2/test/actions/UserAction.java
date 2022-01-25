793c1e0d70b1c6ca15a6896e27b38e056efd00a8
PRITI JANA <PRITI JANA@LAPTOP-2R6TUTEP>
2022-01-25 11:56:34 +0530

    initial commit

---
+++ src/java/struts2/test/actions/UserAction.java
@@ -0,0 +1,302 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Struts2/ActionSupport.java to edit this template
+ */
+package struts2.test.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import struts2.test.beans.User;
+import struts2.test.dao.Admin;
+
+/**
+ *
+ * @author PRAVAT KUMAR JANA
+ */
+public class UserAction extends ActionSupport {
+
+    private String userName;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private boolean validUser;
+    private boolean status;
+
+    private String msg = "";
+    Admin admin = null;
+    private int ctr = 0;
+
+    public String registerUser() throws Exception {
+        admin = new Admin();
+
+        try {
+            setCtr(admin.registerUser(userName, password, firstName, lastName,
+                    email, phoneNumber));
+            if (getCtr() > 0) {
+                setMsg("Registration Successfull");
+            } else {
+                setMsg("Some error");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "REGISTER";
+    }
+
+    ResultSet rs = null;
+
+    private List<User> userList = null;
+    private boolean noData = false;
+    Admin a = new Admin();
+
+    public String showUser() throws Exception {
+
+        try {
+            setUserList(new ArrayList<>());
+            setUserList(a.report());
+
+            if (!userList.isEmpty()) {
+                setNoData(false);
+                System.out.println("Users retrieve = " + getUserList().size());
+                System.out.println("setting nodata=false");
+            } else {
+                setNoData(true);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "REPORT";
+    }
+    
+    Admin dao = new Admin();
+    private String submitType;
+    public String updateUser() throws Exception {
+
+        try {
+            if (submitType.equals("updatedata")) {
+                User user = dao.fetchUserDetails(userName.trim());
+                if (user != null) {
+                    userName = user.getUserName();
+                    password = user.getPassword();
+                    firstName = user.getFirstName();
+                    lastName = user.getLastName();
+                    email = user.getEmail();
+                    phoneNumber = user.getPhoneNumber();
+                }
+            } else {
+                int i = dao.updateUserDetails(userName, password, firstName, lastName,
+                        email, phoneNumber);
+                if (i > 0) {
+                    msg = "Record Updated Successfuly";
+                } else {
+                    msg = "error";
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "UPDATE";
+    }
+    Admin a1=new Admin();
+    public String deleteUser() throws Exception {
+        try {
+            int isDeleted = a1.deleteUserDetails(userName);
+            if (isDeleted > 0) {
+                msg = "Record deleted successfully";
+            } else {
+                msg = "Some error";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "DELETE";
+    }
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the phoneNumber
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @param phoneNumber the phoneNumber to set
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * @return the validUser
+     */
+    public boolean isValidUser() {
+        return validUser;
+    }
+
+    /**
+     * @param validUser the validUser to set
+     */
+    public void setValidUser(boolean validUser) {
+        this.validUser = validUser;
+    }
+
+    /**
+     * @return the status
+     */
+    public boolean isStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the msg
+     */
+    public String getMsg() {
+        return msg;
+    }
+
+    /**
+     * @param msg the msg to set
+     */
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    /**
+     * @return the ctr
+     */
+    public int getCtr() {
+        return ctr;
+    }
+
+    /**
+     * @param ctr the ctr to set
+     */
+    public void setCtr(int ctr) {
+        this.ctr = ctr;
+    }
+
+    /**
+     * @return the userList
+     */
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    /**
+     * @param userList the userList to set
+     */
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
+
+    /**
+     * @return the noData
+     */
+    public boolean isNoData() {
+        return noData;
+    }
+
+    /**
+     * @param noData the noData to set
+     */
+    public void setNoData(boolean noData) {
+        this.noData = noData;
+    }
+
+    /**
+     * @return the submitType
+     */
+    public String getSubmitType() {
+        return submitType;
+    }
+
+    /**
+     * @param submitType the submitType to set
+     */
+    public void setSubmitType(String submitType) {
+        this.submitType = submitType;
+    }
+
+}
